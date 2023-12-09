GitMirroring/gnuboot

commit cabc1cac08cb41777caa5674a1fd229d1e89bceb

website-build: avoid unnecessary git fetch
When we already have the right untitled revision checked out, we don't
need to try to fetch its revision again. This also enable offline
builds in that case.

Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
Acked-by: Adrien 'neox' Bourmault <neox@gnu.org>

website-build/build.sh
@@ -51,12 +51,17 @@ sync_repo()
 			git -C "${dst_path}" checkout "${src_revision}"
 		fi
 	elif [ -z "${src_path}" ] ; then
+		localrev="$(git --no-pager log --oneline HEAD -1 --format='%H')"
+
 		git -C "${dst_path}" remote get-url origin || \
 		    git -C "${dst_path}" remote add origin "${src_uri}"
 		git -C "${dst_path}" remote set-url origin "${src_uri}"
 		git -C "${dst_path}" clean -dfx
-		git -C "${dst_path}" fetch origin
-		git -C "${dst_path}" checkout "${src_revision}"
+
+		if [ "${localrev}" != "${src_revision}" ] ; then
+			git -C "${dst_path}" fetch origin
+			git -C "${dst_path}" checkout "${src_revision}"
+		fi
 
 	else
 		rm -rf "${dst_path}"

commit 28247e6aa1cae82571431b2a146c1f8326f607f6

website-build: build.sh: fix offline builds.
Without that fix, build.sh compares the desired Untitled revision with
the GNU Boot git revision, so the comparison always fail, which leads
to broken offline builds.

This was broken from the start in the commit
cabc1cac08cb41777caa5674a1fd229d1e89bceb ("website-build: avoid
unnecessary git fetch").

Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
Acked-by: Adrien 'neox' Bourmault <neox@gnu.org>

website-build/build.sh
@@ -51,7 +51,8 @@ sync_repo()
 			git -C "${dst_path}" checkout "${src_revision}"
 		fi
 	elif [ -z "${src_path}" ] ; then
-		localrev="$(git --no-pager log --oneline HEAD -1 --format='%H')"
+		localrev="$(git -C "${dst_path}" --no-pager \
+				log --oneline HEAD -1 --format='%H')"
 
 		git -C "${dst_path}" remote get-url origin || \
 		    git -C "${dst_path}" remote add origin "${src_uri}"