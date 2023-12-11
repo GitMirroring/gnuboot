GitMirroring/gnuboot

commit 857afa42a8ade870115391b09d712b110e6a1066

Switch to packages structure.
The various build scripts are scattered around in multiple
places. This make it hard for contributors to understand what they
need to modify.

Most GNU Boot users are interested in running GNU/Linux or BSD
operating systems. And the way to install software on these
operating systems is through a package manager. So most users and
contributors already know the package manager abstraction.

So using that abstraction makes it easier to find where things are.

The scripts to install dependencies don't really fit the new structure
but for now we move them in to make sure that everything works
fine. This could be fixed later on and migrated to a single
dependencies packages by auto-detecting the distribution with
/etc/os-release.

Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
Acked-by: Adrien 'neox' Bourmault <neox@gnu.org>
Signed-off-by: Adrien 'neox' Bourmault <neox@gnu.org>

build
@@ -27,19 +27,41 @@ set -u -e
 
 projectname="$(cat projectname)"
 
-build=./resources/scripts/build
+# This is for backward compatibility: before the list of "modes" was
+# auto-detected, but there isn't a 1:1 matching between modes and
+# packages tasks (build, clean, etc).
+tasks="\
+	boot \
+	clean \
+	dependencies \
+	descriptors \
+	module \
+	payload \
+	release \
+"
 
 list_tasks() {
-	for task in "${build}"/*; do
-		printf '%s\n' "${task##*/}"
+	for task in ${tasks} ; do
+		echo "${task}"
 	done
+}
+
+list_tasks_paths() {
+	task="${1}"
+
+	find resources/packages \
+		-mindepth 2 -maxdepth 2 \
+		-type f \
+		-name "${task}" \
+		-printf "%P\n"
 }
 
 # Takes exactly one task as parameter
 list_packages() {
-	for package in "${build}"/"${1}"/*; do
-		printf '%s\n' "${package##*/}"
-	done
+	task="${1}"
+
+	list_tasks_paths "${task}" | \
+		sed 's#/.*##'
 }
 
 help() {
@@ -82,18 +104,26 @@ if [ $# -gt 1 ]; then
 
 	case "${package}" in
 		list)
-			printf "Available packages for task '%s':\n\n" "${task}"
-			list_packages "${task}"
+			tasks_paths=$(list_tasks_paths "${task}")
+			if [ -z "${tasks_paths}" ] ; then
+				die "Invalid task '${task}'." \
+				    " See './build help'."
+			else
+				printf "Available packages for task '%s':\n\n" \
+				   "${task}"
+				list_packages "${task}"
+			fi
 			;;
 		all)
 			for package in $(list_packages "${task}"); do
-				"${build}"/"${task}"/"${package}" $@
+				resources/packages/"${package}"/"${task}" $@
 			done
 			;;
 		*)
-			if [ -d "${build}"/"${task}"/ ]; then
-				if [ -f "${build}"/"${task}"/"${package}" ]; then
-					"${build}"/"${task}"/"${package}" $@
+			if [ -d resources/packages/"${package}" ] ; then
+				pkg_dir=resources/packages/"${package}"
+				if [ -f "${pkg_dir}"/"${task}" ]; then
+					"${pkg_dir}"/"${task}" $@
 				else
 					help
 					die "Invalid package for '${task}'." \

download
@@ -36,12 +36,10 @@ deleteblobs="true"
 
 rm -f "build_error"
 
-download=resources/scripts/download
-
 list_packages() {
-	for package in "${download}"/*; do
-		printf '%s\n' "${package##*/}"
-	done
+	ls -d resources/packages/*/download | \
+	    sed 's#/download$##' | \
+	    sed 's#^resources/packages/##'
 }
 
 help() {
@@ -85,32 +83,30 @@ shift 1
 [ "${package}" = help ] && help && exit 0
 
 if [ "${package}" = "all" ]; then
-	for downloadPackage in ${download}/*; do
-		if [ -f "${downloadPackage}" ]; then
-			if [ "${deleteblobs}" = "false" ]; then
-				NODELETE= "${downloadPackage}"
-			else
-				"${downloadPackage}"
-			fi
+	for script in resources/packages/*/download; do
+		if [ "${deleteblobs}" = "false" ]; then
+			NODELETE= "${script}"
+		else
+			"${script}"
 		fi
 	done
 	exit 0
-elif [ ! -f "${download}/${package}" ]; then
+elif [ ! -f "resources/packages/${package}/download" ]; then
 	help
-	die "Invalid argument '${package}'. See: './download help'."
+	die "Invalid package '${package}'. See: './download help'."
 fi
 
 if [ $# -lt 1 ]; then
 	if [ "${deleteblobs}" = "false" ]; then
-		NODELETE= "${download}/${package}"
+		NODELETE= resources/packages/"${package}"/download
 	else
-		"${download}/${package}"
+		resources/packages/"${package}"/download
 	fi
 else
 	if [ "${deleteblobs}" = "false" ]; then
-		NODELETE= "${download}/${package}" $@
+		NODELETE= resources/packages/"${package}"/download $@
 	else
-		"${download}/${package}" $@
+		resources/packages/"${package}"/download $@
 	fi
 fi
 

modify
@@ -26,19 +26,25 @@ set -u -e
 projectname="$(cat projectname)"
 ./resources/scripts/misc/versioncheck
 
-modify=./resources/scripts/modify
-
+list_modify_paths() {
+	find resources/packages \
+		-mindepth 2 -maxdepth 2 \
+		-type d \
+		-name "modify" \
+		-printf "%P\n"
+}
 list_packages() {
-	for package in "${modify}"/*; do
-		printf '%s\n' "${package##*/}"
-	done
+	list_modify_paths | \
+		sed 's#/.*##'
 }
 
 # Takes exactly one package as parameter
 listoptions() {
-	for option in "${modify}"/"${1}"/*; do
-		printf '%s\n' "${option##*/}"
-	done
+	package="${1}"
+	find resources/packages/"${package}/modify/" \
+		-mindepth 1 -maxdepth 1 \
+		-type f \
+	        -printf "%P\n"
 }
 
 help() {
@@ -75,18 +81,25 @@ if [ $# -gt 1 ]; then
 
 	case "${option}" in
 	list)
-		printf "Available options for package '%s':\n\n" "${package}"
-		listoptions "${package}"
+		if [ ! -d resources/packages/"${package}" ] ; then
+			die "Invalid package '${package}'." \
+			    " See './modify help'."
+		else
+			printf "Available options for package '%s':\n\n" \
+			   "${package}"
+			listoptions "${package}"
+		fi
 		;;
 	all)
 		for option in $(listoptions "${package}"); do
-			"${modify}"/"${package}"/"${option}" $@
+			resources/packages/"${package}"/modify/"${option}" $@
 		done
 		;;
 	*)
-		if [ -d "${modify}"/"${package}"/ ]; then
-			if [ -f "${modify}"/"${package}"/"${option}" ]; then
-				"${modify}"/"${package}"/"${option}" $@
+		if [ -d resources/packages/"${package}"/modify ]; then
+			pkg_dir=resources/packages/"${package}"
+			if [ -f "${pkg_dir}"/modify/"${option}" ]; then
+				"${pkg_dir}"/modify/"${option}" $@
 			else
 				help
 				die "Invalid option for '${package}'." \

commit 5b9dd7adba9f0acc0faf14fe723e1f72e3afbec4

packages: fix calls to build descriptors.
Without that fix building images with make release or './build release
all' results in the following error:
    [...]
    Built lenovo/t400 (ThinkPad R400)
    make[1]: Leaving directory '/home/gnutoo/gnuboot/coreboot/default'

    Creating new ROM image: bin/r400_16mb/seabios_withgrub_r400_16mb_libgfxinit_corebootfb_colemak.rom
    Usage:
            ./build <TASK> <PACKAGE>
            ./build --help
    [...]
    Refer to the gnuboot documentation for more information.
    Error: Invalid task 'descriptors'.
    Error:  See './build --help'.
    Error: build/roms: something went wrong
    make: *** [Makefile:47: release] Error 1

This was broken by the commit 857afa42a8ade870115391b09d712b110e6a1066
("Switch to packages structure.").

Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
Acked-by: Adrien 'neox' Bourmault <neox@gnu.org>

Makefile
@@ -34,7 +34,7 @@ modules:
 	./build module all
 
 ich9m-descriptors:
-	./build descriptors ich9m
+	./build build descriptors
 
 payloads:
 	./build payload all

build
@@ -36,7 +36,6 @@ tasks="\
 	boot \
 	clean \
 	dependencies \
-	descriptors \
 	distclean \
 	install \
 	module \

resources/packages/roms_helper/boot
@@ -241,13 +241,13 @@ moverom() {
 	for romsize in 4 8 16; do
 		if [ "${cuttype}" = "${romsize}MiB ICH9 IFD NOR flash" ]; then
 			if [ ! -f "descriptors/ich9m/ich9fdgbe_${romsize}m.bin" ]; then
-				./build descriptors ich9m
+				./build build descriptors
 			fi
 			dd if=descriptors/ich9m/ich9fdgbe_${romsize}m.bin of=${newrompath} bs=1 count=12k conv=notrunc
 		fi
 		if [ "${cuttype}" = "${romsize}MiB ICH9 IFD NOGBE NOR flash" ]; then
 			if [ ! -f "descriptors/ich9m/ich9fdnogbe_${romsize}m.bin" ]; then
-				./build descriptors ich9m
+				./build build descriptors
 			fi
 			dd if=descriptors/ich9m/ich9fdnogbe_${romsize}m.bin of=${newrompath} bs=1 count=4k conv=notrunc
 		fi