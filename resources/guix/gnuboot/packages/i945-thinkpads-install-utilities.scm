45a20231b70725b67edc65b2652ef23253e0d7ca
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2024-11-25 16:05:46 +0100

    guix: i945-thinkpads-install-utilities: fix copyright header.
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- resources/guix/gnuboot/packages/i945-thinkpads-install-utilities.scm
+++ resources/guix/gnuboot/packages/i945-thinkpads-install-utilities.scm
@@ -14,7 +14,7 @@
 ;;; GNU General Public License for more details.
 ;;;
 ;;; You should have received a copy of the GNU General Public License
-;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+;;; along with GNU Boot.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnuboot packages i945-thinkpads-install-utilities)
   #:use-module (gnu packages)
