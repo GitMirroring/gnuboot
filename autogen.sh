29d0d83872846289f4e70ab2e40731109ddaec0c
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2025-01-31 02:32:24 +0100

    tests: lint: shellcheck: test for bash.
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # Copyright (C) 2023 Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
 #
 # This program is free software: you can redistribute it and/or modify
--- website/serve.sh
+++ website/serve.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # Copyright (C) 2022-2024 Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
 #
 # This program is free software: you can redistribute it and/or modify
