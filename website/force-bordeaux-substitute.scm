c222591cafc0719e84444224edf6a339d6aeb8fe
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2026-03-18 03:31:13 +0100

    website: force-bordeaux-substitutes: fix copyright notice.
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- website/force-bordeaux-substitute.scm
+++ website/force-bordeaux-substitute.scm
@@ -3,18 +3,18 @@
 ;;;
 ;;; This file is part of GNU Boot.
 ;;;
-;;; This file is free software; you can redistribute it and/or modify it
-;;; under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 3 of the License, or (at
-;;; your option) any later version.
+;;; GNU Boot is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published
+;;; by the Free Software Foundation; either version 3 of the License,
+;;; or (at your option) any later version.
 ;;;
-;;; This file is distributed in the hope that it will be useful, but
+;;; GNU Boot is distributed in the hope that it will be useful, but
 ;;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;; GNU General Public License for more details.
 ;;;
 ;;; You should have received a copy of the GNU General Public License
-;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+;;; along with GNU Boot.  If not, see <http://www.gnu.org/licenses/>.
 
 (use-modules (gcrypt pk-crypto))
 (use-modules (guix pki))
