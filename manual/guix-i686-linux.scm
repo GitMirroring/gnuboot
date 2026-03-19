9ad1405f031b9277502857d651dfb4114517ec3e
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2026-03-19 15:15:23 +0100

    manual: guix-*-linux.scm: clarify license and add copyright.
    The license is already stated in the manual, especially in the various
    versions being generated (PDF, HTML, etc) that also contain these
    shell files.
    
    However since the license in these files don't reflect that it could
    confuse people.
    
    In addition I added the missing copyright as it's a good practice,
    especially because these files are meant to be copied out of the
    manual.
    
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- manual/guix-i686-linux.scm
+++ manual/guix-i686-linux.scm
@@ -1,15 +1,21 @@
-;;; This file is free software; you can redistribute it and/or modify it
-;;; under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 3 of the License, or (at
-;;; your option) any later version.
+;;; Copyright (C) 2025 Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
 ;;;
-;;; This file is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; This file is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version, or (at your option) under the
+;;; terms of the GNU Free Documentation License, Version 1.3 or any
+;;; later version published by the Free Software Foundation.
+;;;
+;;; This file is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;; GNU General Public License for more details.
 ;;;
 ;;; You should have received a copy of the GNU General Public License
-;;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+;;; and the GNU Free Documentation License along with this file.  If
+;;; not, see <https://www.gnu.org/licenses/>.
+
 (use-modules (gnu) (gnu bootloader) (gnu packages linux))
 (operating-system
   (host-name "guix-i686-linux")
