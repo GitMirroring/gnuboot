f556892d94d9fda9cac0c8ff25b474f76b7cdab9
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2026-03-21 01:53:24 +0100

    checkpatch.scm: usage: break line over 80 characters.
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- scripts/checkpatch.scm
+++ scripts/checkpatch.scm
@@ -2020,7 +2020,9 @@ character argument, it can also works on different tables or line formats."
             "\n"
             "Options:\n"
             "\t--help print this help.\n"
-            "\t-f     don't treat FILE as a patch, but as regular source file instead.\n"))
+            "\t-f     "
+            "don't treat FILE as a patch, but as regular source file instead"
+            ".\n"))
   (exit exit-code))
 
 (define (main args)
