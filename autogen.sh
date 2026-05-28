7869bb4b2792c070d8fd0f3422b44fb9c0108c43
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2026-05-28 19:04:21 +0200

    autogen.sh: fix PureOS 10 (byzantium)
    Without this, with a clean checkout './autogen.sh' produces the
    following:
    
        [...]
        configure.ac:17: installing './install-sh'
        configure.ac:103: error: required file './ltmain.sh' not found
        configure.ac:17: installing './missing'
        manual/Makefile.am:15: installing 'manual/mdate-sh'
        manual/Makefile.am:15: installing 'manual/texinfo.tex'
        autoreconf: automake failed with exit status: 1
    
    However running ./autogen.sh a second time fixes the issue.
    
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- autogen.sh
+++ autogen.sh
@@ -14,4 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-autoreconf -vfi "$@"
+# On some distributions (for instance PureOS 10 (byzantium)), we need
+# to run autoreconf twice, othrewise it fails.
+autoreconf -vfi "$@" || autoreconf -vfi "$@"
